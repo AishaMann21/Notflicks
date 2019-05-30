dbf6ed8da88fdc6ada1344c7798109c54c3cecd3
Aisha Mann <aishamann21@gmail.com>
2019-05-30 03:37:51 +0000

    added title

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 $("#searchbutton").click(function(){
   var movieName = $("input").val();
-  console.log(movieName);
+
   
   
     $.ajax({
         url: "https://www.omdbapi.com/?apikey=8e9ded79&s=" + movieName,
         method: "GET",
         success: function(response) {
+            var movieTitle = response.Search[0].Title
+            $("#title").html(movieTitle);
             var movieURL= response.Search[0].Poster
             $("#poster").attr("src", movieURL);
+           
 
         },
     }); 
