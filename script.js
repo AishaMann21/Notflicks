2f6c4e6a03cc979fce05f6170033a4afff371932
Aisha Mann <aishamann21@gmail.com>
2019-05-30 22:19:30 +0000

    finished

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 $("#searchbutton").click(function(){
   var movieName = $("input").val();
-
-  
-  
     $.ajax({
         url: "https://www.omdbapi.com/?apikey=8e9ded79&s=" + movieName,
         method: "GET",
@@ -11,9 +8,20 @@ $("#searchbutton").click(function(){
             $("#title").html(movieTitle);
             var movieURL= response.Search[0].Poster
             $("#poster").attr("src", movieURL);
-           
-
-        },
+            var movieIMdb= response.Search[0].imdbID
+            $("#link").attr("href", "https://www.imdb.com/title/" + movieIMdb);
+            $("#link").html("Click here for more info!!");
+            $("#recommended").html("More Like This:");
+            
+            var movieURL= response.Search[1].Poster
+            $("#poster1").attr("src", movieURL);
+            
+            var movieURL= response.Search[2].Poster
+            $("#poster2").attr("src", movieURL);
+            
+            var movieURL= response.Search[3].Poster
+            $("#poster3").attr("src", movieURL);
+        }
     }); 
     
 });
